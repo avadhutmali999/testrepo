ebfac59a6b5a0992da47eeb1de7a59f3cb631850
Avadhut Mali <avadhutmali999@gmail.com>
2022-07-20 15:12:00 +0530

    first commit

---
+++ sumtestfile.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream.h>
+
+void main();
+
+{
+int a, int b, int c;
+cout<<"Enter variable a\t";
+cin>>a;
+cout<<"Enter variable b\t";
+cin>>b;
+cout<<"\n Sum is a \t";
+cout<<a+b;
+}
